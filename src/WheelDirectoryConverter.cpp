3eea30268956ae53b9e5dec34c250aa9a710aa41
fzhiheng <71751306+fzhiheng@users.noreply.github.com>
2023-06-15 23:46:50 +0800

    fix:修复了转wheel单位的错误

--- src/WheelDirectoryConverter.cpp
+++ src/WheelDirectoryConverter.cpp
@@ -1,6 +1,7 @@
 #include "nuscenes2bag/WheelDirectoryConverter.hpp"
 #include "nuscenes2bag/utils.hpp"
 #include <thread>
+#define pi 3.14159265358979323846
 
 namespace nuscenes2bag {
 
@@ -9,8 +10,16 @@ namespace nuscenes2bag {
         try {
             nav_msgs::Odometry msg;
             msg.header = std_msgs::Header();
+            auto RL_wheel_speed = wheelData.RL_wheel_speed;
+            auto RR_wheel_speed = wheelData.RR_wheel_speed;
+
+            // 将速度转成m/s
+            double radius = 0.305; // Known Zoe wheel radius in meters.
+            double circumference = 2 * pi * radius;
+            RL_wheel_speed *= circumference / 60;
+            RR_wheel_speed *= circumference / 60;
 //            msg.header.stamp = ros::Time().fromSec(data.header.stamp.sec + data.header.stamp.nsec / 1e9);
-            msg.twist.twist.linear.x = 0.5*(wheelData.RL_wheel_speed + wheelData.RR_wheel_speed);
+            msg.twist.twist.linear.x = 0.5*(RL_wheel_speed + RR_wheel_speed);
             msg.twist.twist.linear.y = 0;
             msg.twist.twist.linear.z = 0;
             msg.twist.twist.angular.x = 0;
