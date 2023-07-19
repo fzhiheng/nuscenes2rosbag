66bd7ec4a418c96db08d720ab049f474e8d02391
fzhiheng <71751306+fzhiheng@users.noreply.github.com>
2023-07-19 14:46:47 +0800

    feat:轮速计加入转向角速度信息

--- src/WheelDirectoryConverter.cpp
+++ src/WheelDirectoryConverter.cpp
@@ -2,6 +2,9 @@
 #include "nuscenes2bag/utils.hpp"
 #include <thread>
 #define pi 3.14159265358979323846
+#define radius 0.305 // Known Zoe wheel radius in meters.
+#define steer_radio 16.6// Zoe steer radio
+#define wheel_base 2.588 // Zoe wheel base
 
 namespace nuscenes2bag {
 
@@ -12,25 +15,30 @@ namespace nuscenes2bag {
             msg.header = std_msgs::Header();
             auto RL_wheel_speed = wheelData.RL_wheel_speed;
             auto RR_wheel_speed = wheelData.RR_wheel_speed;
+            auto steer_corrected = wheelData.steer_corrected;
 
             // 将速度转成m/s
-            double radius = 0.305; // Known Zoe wheel radius in meters.
             double circumference = 2 * pi * radius;
             RL_wheel_speed *= circumference / 60;
             RR_wheel_speed *= circumference / 60;
+            double vx = 0.5*(RL_wheel_speed + RR_wheel_speed);
+
+            // 将转角转成弧度
+            steer_corrected = steer_corrected * pi / (180 * steer_radio);
+            double twist_z =  vx * tan(steer_corrected) / wheel_base;
+
 //            msg.header.stamp = ros::Time().fromSec(data.header.stamp.sec + data.header.stamp.nsec / 1e9);
-            msg.twist.twist.linear.x = 0.5*(RL_wheel_speed + RR_wheel_speed);
+            msg.twist.twist.linear.x = vx;
             msg.twist.twist.linear.y = 0;
             msg.twist.twist.linear.z = 0;
             msg.twist.twist.angular.x = 0;
             msg.twist.twist.angular.y = 0;
-            msg.twist.twist.angular.z = 0;
+            msg.twist.twist.angular.z = twist_z;
             return boost::optional<nav_msgs::Odometry>(msg);
 
         } catch (const std::exception &e) {
             PRINT_EXCEPTION(e);
         }
-
         return boost::none;
     }
 
